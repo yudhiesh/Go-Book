e1a4b43c1ec0d209833763996303db8895d13e80
Yudhiesh Ravindranath <yudhiesh1997@gmail.com>
2021-03-24 00:39:52 +0800

    Disabled Directory Listings

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 func main() {
@@ -13,9 +14,22 @@ func main() {
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux.Handle("/static/", http.StripPrefix("/static", neuter(fileServer)))
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
+}
+
+// Disable http.FileServer Directory Listings
+// NOTE: Prevent navigable directory listings and disable it all together
+func neuter(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasSuffix(r.URL.Path, "/") {
+			http.NotFound(w, r)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }

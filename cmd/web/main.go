d4d94f685346c9f3aaf4ab39b08083cd751e017b
Yudhiesh Ravindranath <yudhiesh1997@gmail.com>
2021-03-24 20:39:10 +0800

    Chapter 3.5

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,12 +5,13 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strings"
 )
 
 // Configurations struct that stores all the flags that can be passed when
 // running the server
-type Config struct {
+var Cfg Configurations
+
+type Configurations struct {
 	Addr      string
 	StaticDir string
 }
@@ -32,10 +33,10 @@ func main() {
 
 	// This parses the command-line flag.
 	// This needs to be called before using the flag variables such as addr
-	cfg := new(Config)
+	// cfg := new(Config)
 	// As the strings are stored in a struct we can access them using
 	// flag.StringVar() instead of flag.String()
-
+	cfg := Configurations{}
 	flag.StringVar(&cfg.Addr, "addr", ":4000", "HTTP network address")
 	flag.StringVar(&cfg.StaticDir, "static-dir", "./ui/static", "Path to static assets")
 
@@ -57,23 +58,6 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	mux := http.NewServeMux()
-
-	// HandleFunc takes in normal functions that are not actually Handlers as
-	// they do not have the method ServeHTTP
-	mux.HandleFunc("/", app.home)
-	// NOTE: If you wanted to turn home into an actual handler you would need to
-	// instantiate an interface home and then turn the home function into a
-	// ServeHTTP method
-	// Then pass it by pointer as below:
-	// mux.Handle("/", &home{})
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-
-	fileServer := http.FileServer(http.Dir(cfg.StaticDir))
-
-	mux.Handle("/static/", http.StripPrefix("/static", neuter(fileServer)))
-
 	// Override the http.Server Error Log
 	// By default if Go's HTTP server encounters an error it will log it using
 	// the standard logger
@@ -82,23 +66,10 @@ func main() {
 	srv := &http.Server{
 		Addr:     cfg.Addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	infoLog.Printf("Starting server on %s", cfg.Addr)
 	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
-}
-
-// Disable http.FileServer Directory Listings
-// NOTE: Prevent navigable directory listings and disable it all together
-func neuter(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if strings.HasSuffix(r.URL.Path, "/") {
-			http.NotFound(w, r)
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	})
 }
---
+++ cmd/web/routes.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+)
+
+func (app *application) routes() *http.ServeMux {
+
+	mux := http.NewServeMux()
+
+	// HandleFunc takes in normal functions that are not actually Handlers as
+	// they do not have the method ServeHTTP
+	mux.HandleFunc("/", app.home)
+	// NOTE: If you wanted to turn home into an actual handler you would need to
+	// instantiate an interface home and then turn the home function into a
+	// ServeHTTP method
+	// Then pass it by pointer as below:
+	// mux.Handle("/", &home{})
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
+
+	fileServer := http.FileServer(http.Dir(Cfg.StaticDir))
+
+	mux.Handle("/static/", http.StripPrefix("/static", neuter(fileServer)))
+
+	return mux
+}
+
+// Disable http.FileServer Directory Listings
+// NOTE: Prevent navigable directory listings and disable it all together
+func neuter(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasSuffix(r.URL.Path, "/") {
+			http.NotFound(w, r)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}

01ac7e271bc6f5971ce48cf68c1dd87e70de4eaf
Yudhiesh Ravindranath <yudhiesh1997@gmail.com>
2021-04-06 00:41:13 +0800

    Chapter 8.3

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 	"yudhiesh/snippetbox/pkg/models"
 )
 
@@ -57,11 +59,43 @@ func (app *Application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	content := r.PostForm.Get("content")
 	expires := r.PostForm.Get("expires")
 
-	// For all request types
-	// Contains the form data, request body and any query string parameters
-	// title := r.Form.Get("title")
-	// content := r.Form.Get("content")
-	// expires := r.Form.Get("expires")
+	errors := make(map[string]string)
+
+	// Check that the title field is not blank and that the title does not have
+	// more than 100 characters
+	if strings.TrimSpace(title) == "" {
+		errors["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(title) > 100 {
+		// We use RuneCountInString() instead of len as we want to check the
+		// number of characters and not the length
+		// Words such as "Zoë" has 3 characters but 4 bytes as the umlaut
+		// contains an additional byte
+		errors["title"] = "This field is too long (maximum is 100 characters)"
+	}
+
+	// Check that the content is not blank
+	if strings.TrimSpace(content) == "" {
+		errors["content"] = "This field cannot be blank"
+	}
+
+	// Check that the expires field is not blank and that it is of a valid
+	// number of days
+	if strings.TrimSpace(expires) == "" {
+		errors["expires"] = "This field cannot be blank!"
+	} else if expires != "365" && expires != "7" && expires != "1" {
+		errors["expires"] = "This field is invalid"
+	}
+
+	// If there are any validation errors, re-display the create.page.tmpl
+	// template passing in the validation errors and the previously submitted
+	// form data
+	if len(errors) > 0 {
+		app.render(w, r, "create.page.tmpl", &templateData{
+			FormErrors: errors,
+			FormData:   r.PostForm,
+		})
+		return
+	}
 
 	// Pass the data to the SnippetModel.Insert() method, receiving the
 	// ID of the new record back.
